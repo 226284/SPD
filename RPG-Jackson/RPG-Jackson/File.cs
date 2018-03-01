655152fc0dafeb513a605a91f97799fbc394e3f0
Jakub Woszczyna <kubwosz@gmail.com>
2018-03-01 22:29:16 +0100

    Pierwszy program z labek

---
+++ RPG-Jackson/RPG-Jackson/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Jackson
+{
+    class File
+    {
+        public int numberOfTasks;
+        public List<Task> listOfTasks;
+
+        //public void Segregation()
+        //{
+        //    foreach(Task t in listOfTasks)
+        //    {
+               
+        //    }
+        //}
+    }
+}
