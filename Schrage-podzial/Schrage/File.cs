22487561d0e957ce6741d527dcc060f7009e81f9
Michał Wieczorek <michal126@hotmail.com>
2018-03-21 13:40:16 +0100

    inicjalizacja schrage z podziałem

---
+++ Schrage-podzial/Schrage/File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace Schrage
+{
+    class File
+    {
+        public int numberOfTasks;
+        public List<Task> listOfTasks;
+        //public SimplePriorityQueue<Task> queueOfTasks;
+        //public void Segregation()
+        //{
+        //    foreach(Task t in listOfTasks)
+        //    {
+
+        //    }
+        //}
+    }
+}
