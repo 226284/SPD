400589678f19ac406d3cf6b4232f795c4f9469a9
Michał Wieczorek <michal126@hotmail.com>
2018-03-21 08:58:36 +0100

    prosze dzialaj

---
+++ Schrage/Schrage/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrege
+{
+    class Task
+    {
+
+        public int r; //termin dostępności
+        public int p; //czas obsługi
+        public int q; //czas dostarczenia
+
+
+        public Task(int R, int P, int Q)
+        {
+            r = R;
+            p = P;
+            q = Q;
+        }
+    }
+}
