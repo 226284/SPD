4366ecf9283bd582c88903d5a24c22a1687865be
Michał Wieczorek <michal126@hotmail.com>
2018-03-21 09:00:23 +0100

    kompile

--- Schrage/Schrage/File.cs
+++ Schrage/Schrage/File.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Priority_Queue;
 
-namespace Schrege
+namespace Schrage
 {
     class File
     {
--- Schrage/Schrage/Task.cs
+++ Schrage/Schrage/Task.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Schrege
+namespace Schrage
 {
     class Task
     {
